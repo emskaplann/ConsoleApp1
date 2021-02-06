6755512b80d2c60a08f271a7658d0172c664b13f
Emirhan Kaplan <emskaplann@gmail.com>
2021-02-06 12:40:10 -0500

    files added.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+            else
+                Console.WriteLine("The answer is not greater than 10.");
+        }
+    }
+}
